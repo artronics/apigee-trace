f52f63ee494a7ceb889297c2b5e9ca8669fb7761
jalal <jalalhosseiny@gmail.com>
2021-05-03 15:37:17 +0100

    set defualt version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = os.environ['VERSION']
+version = os.getenv('VERSION', '0.0.1')
 
 setuptools.setup(
     name="apigee-trace-apim", # Replace with your own username
